34543a6d61243d4bf114f362c6eb5046c89070b0
mateo-purple <ryota289@icloud.com>
2025-06-15 20:14:31 +0900

    Update download.php

--- download.php
+++ download.php
@@ -1,13 +1,17 @@
 <?php
 $counterFile = "counter.txt";
-$file = 'game.zip';
+$file = 'game.zip'; // ←ここを修正した！
 
+// ファイル存在確認
 if (!file_exists($file)) {
     http_response_code(404);
     exit("ファイルが見つかりません。");
 }
 
-// カウンター処理
+// カウンター処理（排他ロック付き）
+if (!file_exists($counterFile)) {
+    file_put_contents($counterFile, "0");
+}
 $fp = fopen($counterFile, "c+");
 if ($fp && flock($fp, LOCK_EX)) {
     $count = (int)fread($fp, filesize($counterFile));
@@ -20,11 +24,12 @@ if ($fp && flock($fp, LOCK_EX)) {
 }
 fclose($fp);
 
-// ヘッダー送信
+// ダウンロード用ヘッダー出力
 header('Content-Type: application/zip');
 header('Content-Disposition: attachment; filename="game.zip"');
 header('Content-Length: ' . filesize($file));
 
+// 出力バッファのクリアとフラッシュ
 ob_clean();
 flush();
 readfile($file);
