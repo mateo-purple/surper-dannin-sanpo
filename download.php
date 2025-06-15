25b4001dcd7af63be52b3af53357954df0e21c06
mateo-purple <ryota289@icloud.com>
2025-06-15 20:07:25 +0900

    Update download.php

--- download.php
+++ download.php
@@ -1,14 +1,31 @@
 <?php
 $counterFile = "counter.txt";
-if (!file_exists($counterFile)) {
-    file_put_contents($counterFile, "0");
-}
-$count = (int)file_get_contents($counterFile);
-$count++;
-file_put_contents($counterFile, $count);
+$file = 'game.zip'; // ← ここも game.zip に変更
 
-$file = 'games/super-danin-sanpo.zip';
+if (!file_exists($file)) {
+    http_response_code(404);
+    echo "ファイルが見つかりません。";
+    exit;
+}
+
+// カウンター処理（排他ロック付き）
+$fp = fopen($counterFile, "c+");
+if (flock($fp, LOCK_EX)) {
+    $size = filesize($counterFile);
+    $count = $size > 0 ? (int)fread($fp, $size) : 0;
+    $count++;
+    ftruncate($fp, 0);
+    rewind($fp);
+    fwrite($fp, $count);
+    fflush($fp);
+    flock($fp, LOCK_UN);
+}
+fclose($fp);
+
+// ダウンロードヘッダー
 header('Content-Type: application/zip');
-header('Content-Disposition: attachment; filename="super-danin-sanpo.zip"');
+header('Content-Disposition: attachment; filename="game.zip"');
+header('Content-Length: ' . filesize($file));
 readfile($file);
+exit;
 ?>
